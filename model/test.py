98e5f789b3d542fbf0a7013011c22aeabaecf1f5
李天波666 <1726451295@qq.com>
2021-05-27 13:01:11 +0800

    test单元测试

---
+++ model/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from generate import generate_an_fraction
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(add_test(),2)
+
+
+if __name__ == '__main__':
+    unittest.main()generate_an_fraction
